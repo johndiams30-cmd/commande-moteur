e1dec8907f188ff17e0cae6569f077ed8e681167
johndiams30-cmd <johndiams30@gmail.com>
2026-03-10 23:44:49 +0100

    Optim

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-const menu = document.getElementById("menuPrincipal")
-const controle = document.getElementById("panneauControle")
-const ampoule = document.getElementById("ampoule")
-const etat = document.getElementById("etat")
-
-// ouvrir panneau
-function ouvrirControle() {
-    menu.classList.add("hidden")
-    controle.classList.remove("hidden")
-}
-
-// retour menu
-function retourMenu() {
-    controle.classList.add("hidden")
-    menu.classList.remove("hidden")
-}
-
-// allumer
 function allumer() {
 
-    fetch("http://192.168.30.212/on")
+    fetch("http://192.168.30.212/on", {
+        mode: "no-cors"
+    });
 
-    ampoule.classList.remove("off")
-    ampoule.classList.add("on")
+    ampoule.classList.remove("off");
+    ampoule.classList.add("on");
 
-    etat.innerText = "Allumée"
-    etat.style.color = "lime"
+    etat.innerText = "Allumée";
+    etat.style.color = "lime";
+
 }
 
-// eteindre
 function eteindre() {
 
-    fetch("http://192.168.30.212/off")
+    fetch("http://192.168.30.212/off", {
+        mode: "no-cors"
+    });
 
-    ampoule.classList.remove("on")
-    ampoule.classList.add("off")
+    ampoule.classList.remove("on");
+    ampoule.classList.add("off");
 
-    etat.innerText = "Éteinte"
-    etat.style.color = "white"
+    etat.innerText = "Éteinte";
+    etat.style.color = "white";
+
 }
