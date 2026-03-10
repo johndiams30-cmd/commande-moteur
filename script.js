fb15f27e3ee667a8ec7cc36f9db2cfc7d717f6a8
johndiams30-cmd <johndiams30@gmail.com>
2026-03-10 17:22:20 +0100

    Optimisation

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function allumer() {
+const etatLampe = document.getElementById("etat-lampe");
+const ipESP32 = "http://ADRESSE_IP_ESP32"; // Remplacez par l'IP de votre ESP32
 
-    fetch("http://ADRESSE_IP_ESP32/on")
+// Allumer la lampe
+document.getElementById("btn-on").addEventListener("click", () => {
+    fetch(`${ipESP32}/on`)
+        .then(() => etatLampe.textContent = "ON")
+        .catch(() => alert("Erreur : impossible de contacter l'ESP32"));
+});
 
-}
-
-function eteindre() {
-
-    fetch("http://ADRESSE_IP_ESP32/off")
-
-}
+// Éteindre la lampe
+document.getElementById("btn-off").addEventListener("click", () => {
+    fetch(`${ipESP32}/off`)
+        .then(() => etatLampe.textContent = "OFF")
+        .catch(() => alert("Erreur : impossible de contacter l'ESP32"));
+});
