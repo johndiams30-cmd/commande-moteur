c8f0bf2262ba43304cbee540b34031c29e7ae33c
johndiams30-cmd <johndiams30@gmail.com>
2026-03-10 23:57:06 +0100

    OPTIMISATION

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
-const menu = document.getElementById("menuPrincipal");
-const controle = document.getElementById("panneauControle");
-const ampoule = document.getElementById("ampoule");
-const etat = document.getElementById("etat");
-
-// ouvrir panneau de commande
 function ouvrirControle() {
 
-    menu.style.display = "none";
-    controle.style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("controle").style.display = "block";
 
 }
 
-// retour menu principal
 function retourMenu() {
 
-    controle.style.display = "none";
-    menu.style.display = "block";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("controle").style.display = "none";
 
 }
 
-// allumer lampe
 function allumer() {
 
-    fetch("http://192.168.30.212/on", {
-        mode: "no-cors"
-    });
+    fetch("http://192.168.30.212/on", { mode: "no-cors" });
 
-    ampoule.classList.remove("off");
-    ampoule.classList.add("on");
+    document.getElementById("ampoule").classList.remove("off");
+    document.getElementById("ampoule").classList.add("on");
 
-    etat.innerText = "Allumée";
-    etat.style.color = "lime";
+    document.getElementById("etat").innerText = "Allumée";
 
 }
 
-// eteindre lampe
 function eteindre() {
 
-    fetch("http://192.168.30.212/off", {
-        mode: "no-cors"
-    });
+    fetch("http://192.168.30.212/off", { mode: "no-cors" });
 
-    ampoule.classList.remove("on");
-    ampoule.classList.add("off");
+    document.getElementById("ampoule").classList.remove("on");
+    document.getElementById("ampoule").classList.add("off");
 
-    etat.innerText = "Éteinte";
-    etat.style.color = "white";
+    document.getElementById("etat").innerText = "Éteinte";
 
 }
